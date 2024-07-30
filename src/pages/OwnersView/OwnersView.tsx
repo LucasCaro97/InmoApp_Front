9fd85ac8889f9ed07dd0d86ff7b48bd3b646957f
Nazareno <nazarenorosales_@hotmail.com>
2024-07-29 23:50:31 -0300

    Owners view, owners table, form and styles

--- src/pages/OwnersView/OwnersView.tsx
+++ src/pages/OwnersView/OwnersView.tsx
@@ -9,14 +9,17 @@ const OwnersView = (): JSX.Element => {
   };
 
   return (
-    <>
+    <div className="container mx-auto p-4 bg-white shadow-lg rounded-lg">
       <h2 className="text-3xl font-bold mb-4">Propietarios</h2>
-      <button onClick={handleOpenForm}>
+      <button
+        className="bg-green-900 text-slate-300 text-lg px-6 py-1 rounded-xl hover:bg-green-700 transition duration-300"
+        onClick={handleOpenForm}
+      >
         {openForm ? "Cerrar formulario" : "Nuevo Propietario"}
       </button>
       {openForm ? <OwnerForm /> : null}
       <OwnersTable />
-    </>
+    </div>
   );
 };
 export { OwnersView };
